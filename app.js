ae33f8eeb05abaa0a377e5732154fbd4875f1210
GreeneGunnar14 <bryce.greene@students.fhu.edu>
2024-01-26 20:59:12 -0600

    add work from other repository

--- app.js
+++ app.js
@@ -1,37 +1,57 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const PORT = process.env.PORT || "3000"
+const PORT = process.env.PORT || "3000";
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.listen(PORT, ()=> {
-    console.log( `App is running on http://localhost:${PORT}...`)
-})
+app.listen(3000, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
 
 app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
-    res.send("Good Job!")
+app.get("/greet", (req, res) => {
+  const cur_year = new Date().getFullYear();
+  const user_dob = parseInt(req.query.year);
+  const age = cur_year - user_dob;
+  res.send(
+    `Hello, ${req.query.name}!<br>You are ${age - 1} or ${age} years old.`
+  );
+});
 
-})
+app.get("/math/:num1/:op/:num2", (req, res) => {
+  const num1 = parseInt(req.params.num1);
+  const op = req.params.op;
+  const num2 = parseInt(req.params.num2);
 
-// http://localhost:3000/greet?name=kaylee&dob=2002
-app.get('/greet', (req, res)=> {
-    console.log(req.query)
+  let result;
 
-    res.send(`hey, ${req.query.name}`)
-})
+  switch (op) {
+    case "plus":
+      result = num1 + num2;
+      break;
+    case "minus":
+      result = num1 - num2;
+      break;
+    case "times":
+      result = num1 * num2;
+      break;
+    case "dividedby":
+      result = num1 / num2;
+      break;
+    case "tothepowerof":
+      result = num1 ** num2;
+      break;
+    default:
+      result = "Incorrect operator";
+  }
 
-app.get('/math/:num1/:op/:num2', (req, res)=> {
-    console.log( req.params )
-    res.send(`${req.params.num1}`)
-})
+  res.render("math", { result });
+});
 
-app.get('/pandorasbox', (req, res)=> {
-
-    // do the work
-    const message = "DAD JOKE"
-
-    res.render('pandorasbox', {title: "Pandora's Box", message} )
-
-})
+app.get("/pandorasbox", (req, res) => {
+  res.render("pandorasbox", { title: "Pandora's Box" });
+});
